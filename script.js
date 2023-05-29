a58365ae036b7249b10790700861e5f5f3aa7761
Michael McLean <g4m3rm1k3@hotmail.com>
2023-05-29 13:23:08 -0400

    function to calculate fact age

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ console.log('Hello World!');
 
 const btn = document.querySelector('.btn-open');
 btn.addEventListener('click', () => {
-	console.log('CLICK');
 	const form = document.querySelector('.fact-form');
+	if (form.classList.contains('hidden')) {
+		btn.textContent = 'close';
+	} else {
+		btn.textContent = 'share a fact';
+	}
 	form.classList.toggle('hidden');
 });
+
+function calcFactAge(year) {
+	const date = new Date().getFullYear();
+	console.log('The age of the fact is ', date - year);
+	return date - year;
+}
+
+const age1 = calcFactAge(2015);
+console.log(age1);
