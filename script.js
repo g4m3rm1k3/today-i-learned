29b9517268f33d3b682d2181ffd4d44cd9b1abfa
Michael McLean <g4m3rm1k3@hotmail.com>
2023-05-29 20:13:10 -0400

    working with arrays

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ btn.addEventListener('click', () => {
 	}
 	form.classList.toggle('hidden');
 });
+
+const fact = ['Libson is the capital of portugal', 2015, true];
+
+console.log(fact);
+console.log(fact[0]);
+console.log(fact[1]);
+console.log(fact.length);
+const newFact = [...fact, 'society'];
+console.log(newFact);
+
+const factObj = {
+	text: 'Libson is the capital of portugal',
+	isCorrect: true,
+	category: 'society',
+	createSummary: function () {
+		return `The fact ${this.text} is ${this.isCorrect
+			.toString()
+			.toUpperCase()}`;
+	},
+};
+
+console.log(factObj.text);
+console.log(factObj['text']);
+
+const { category, isCorrect } = factObj;
+console.log(category);
+console.log(factObj.createSummary());
