466ed3f1e5351c78590b777499faa5d4fb8db0b8
Michael McLean <g4m3rm1k3@hotmail.com>
2023-05-30 05:47:32 -0400

    added filter and backend

--- script.js
+++ script.js
@@ -8,55 +8,82 @@ btn.addEventListener('click', () => {
 	}
 	form.classList.toggle('hidden');
 });
-
-// const fact = ['Libson is the capital of portugal', 2015, true];
-
-// console.log(fact);
-// console.log(fact[0]);
-// console.log(fact[1]);
-// console.log(fact.length);
-// const newFact = [...fact, 'society'];
-// console.log(newFact);
-
-// const factObj = {
-// 	text: 'Libson is the capital of portugal',
-// 	isCorrect: true,
-// 	category: 'society',
-// 	createSummary: function () {
-// 		return `The fact ${this.text} is ${this.isCorrect
-// 			.toString()
-// 			.toUpperCase()}`;
-// 	},
-// };
-
-// console.log(factObj.text);
-// console.log(factObj['text']);
-
-// const { category, isCorrect } = factObj;
-// console.log(category);
-// console.log(factObj.createSummary());
-
-// [2, 4, 6, 8].forEach((x) => console.log(x));
-// const timesTen = [2, 4, 6, 8].map((x) => x * 10);
-// console.log(timesTen);
-
-const CATEGORIES = [
-	{ name: 'technology', color: '#3b82f6' },
-	{ name: 'science', color: '#16a34a' },
-	{ name: 'finance', color: '#ef4444' },
-	{ name: 'society', color: '#eab308' },
-	{ name: 'entertainment', color: '#db2777' },
-	{ name: 'health', color: '#14b8a6' },
-	{ name: 'history', color: '#f97316' },
-	{ name: 'news', color: '#8b5cf6' },
-];
-
+let filter = '';
 const allCategories = CATEGORIES.map((x) => x.name);
-console.log(allCategories);
+function defFilter(x) {
+	filter = x;
+	loadFacts();
+}
 CATEGORIES.forEach((cat) => {
 	const listItem = document.createElement('li');
 	const lastButton = document.querySelector('.category:last-child');
-	console.log(lastButton);
-	listItem.innerHTML = `<button class="btn btn-category" style="background-color: ${cat.color}; margin-bottom: 16px">${cat.name}</button>`;
+	listItem.innerHTML = `<button class="btn btn-category" onClick="defFilter('${cat.name}')" style="background-color: ${cat.color}; margin-bottom: 16px">${cat.name}</button>`;
 	lastButton.appendChild(listItem);
 });
+
+const factsList = document.querySelector('.facts-list');
+
+// Load data from supabase
+async function loadFacts() {
+	factsList.innerHTML = '';
+	const res = await fetch(
+		'https://kxasevsmwxwkvohajnhl.supabase.co/rest/v1/facts',
+		{
+			headers: {
+				apikey:
+					'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4YXNldnNtd3h3a3ZvaGFqbmhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUzNjEyNDgsImV4cCI6MjAwMDkzNzI0OH0.gk9CFyG_z5LEL8fb4MOImg4aTCYTo6nVIqSS34OdiNs',
+				authorizaiton:
+					'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4YXNldnNtd3h3a3ZvaGFqbmhsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODUzNjEyNDgsImV4cCI6MjAwMDkzNzI0OH0.gk9CFyG_z5LEL8fb4MOImg4aTCYTo6nVIqSS34OdiNs',
+			},
+		}
+	);
+	const data = await res.json();
+	createFactsList(data);
+}
+
+function createFactsList(dataArray) {
+	factsList.innerHTML = '';
+	// create DOM elements
+	const htmlArr = dataArray
+		.filter((x) => {
+			console.log(filter);
+			if (filter !== '') {
+				return x.category.toLowerCase() === filter.toLowerCase();
+			} else {
+				return true;
+			}
+		})
+		.map((fact) => {
+			const backgroundColor = CATEGORIES.find(
+				(cat) => cat.name == fact.category
+			)?.color;
+			return `
+	<li class="fact">
+		<p>${fact.text}
+			<a
+			class="source"
+			rel="noopener"
+			href=${fact.source}
+			target="_blank">(Source)
+			</a>
+		</p>
+		<span
+			class="tag"
+			style="background-color: ${backgroundColor}"
+			>${fact.category}
+		</span>
+		<div class="vote-buttons">
+			<button>👍${fact.votesInteresting}</button>
+			<button>🤯 ${fact.votesMindblowing}</button>
+			<button>⛔️ ${fact.votesFalse}</button>
+		</div>
+	</li>
+						`;
+		});
+	console.log(htmlArr);
+	const html = htmlArr.join('');
+	factsList.insertAdjacentHTML('afterbegin', html);
+}
+// createFactsList(initialFacts, CATEGORIES);
+// createFactsList([{ text: 'MIke' }], CATEGORIES);
+loadFacts();
